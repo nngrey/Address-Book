ae6d0656d1f9ba098252efb02e95378a97719f6a
Drew Seminara & Nathan Grey <student@epicodus.com>
2014-02-24 10:55:21 -0800

    complete first step of address book

--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,10 @@
 describe('Contact', function() {
-  describe('fullName', function () {
-    it('combines first and last name, seperated by a space', function () {
+  describe('fullName', function() {
+    it('combines first and last name, seperated by a space', function() {
       var testContact = Object.create(Contact);
       testContact.firstName = "Bob";
+      testContact.lastName = "Smith";
+      testContact.fullName().should.equal("Bob Smith");
     });
   });
 });
